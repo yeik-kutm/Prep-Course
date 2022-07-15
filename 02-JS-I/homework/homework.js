595ab75d401a1b79c8469f4fbfc208dcfafb3df0
yeik-kutm <ymamayar@unal.edu.co>
2022-07-15 18:54:04 -0500

    Tarea incompleta

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -63,14 +63,14 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return x= y
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return str1 = str2 
 }
 
 function menosQueNoventa(num) {
